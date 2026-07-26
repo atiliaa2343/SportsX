49f52c2b9ea3303a1ed861b8cc8d87c01de4f835
atiliaa2343 <atiliaa.thomas@gmail.com>
2026-07-26 18:44:55 -0400

    fix: there was an issue with too many api requests to the endpoint, so had to refactor the code within the collection file

--- src/data/collect.py
+++ src/data/collect.py
@@ -1,13 +1,15 @@
 import pandas as pd 
-import nba_api
-from nba_api.stats.static import teams 
 from nba_api.stats.endpoints import LeagueGameLog 
-import time  
-import pathlib 
 
-season_list = ["2020", "2021", "2022", "2023"] 
-nba_games = pd.DataFrame() 
-for seasonid in season_list: 
-    season_log = LeagueGameLog(season=seasonid, player_or_team_abbreviation='T')  
-    season_df = season_log.get_data_frames()[0] 
-    nba_games.append(season_df)
+
+#This function calls the nba api to grab season data and all team stats into one dataframe
+def create_nbadataframe(season_list): 
+    nba_games = pd.DataFrame() 
+    for seasonid in season_list: 
+        season_log = LeagueGameLog(season=seasonid, player_or_team_abbreviation='T')  
+        season_df = season_log.get_data_frames()[0] 
+        nba_games = nba_games._append(season_df)
+    return nba_games 
+
+season_list = ["2020-21", "2021-22", "2022-23", "2023-24"]  
+create_nbadataframe(season_list)
