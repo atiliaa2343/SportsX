403b5f77ec16141786653c84f1f099f0b3d6a7fb
atiliaa2343 <atiliaa.thomas@gmail.com>
2026-07-26 00:35:22 -0400

    fix: changed dataframe from only being the hornets and celtic games to include all nba teams

--- src/data/collect.py
+++ src/data/collect.py
@@ -1,34 +1,13 @@
-import pandas 
+import pandas as pd 
 import nba_api
 from nba_api.stats.static import teams 
-from nba_api.stats.endpoints import leaguegamefinder
+from nba_api.stats.endpoints import LeagueGameLog 
+import time  
+import pathlib 
 
-#get hornets teams stats and put into a dataframe
-nba_teams = teams.get_teams() 
-hornets = next((team for team in nba_teams if team["abbreviation"] == "CHA"), None) 
-hornets_id = hornets["id"]
-#query for games where the hornets were playing 
-hornetsgamesfinder = leaguegamefinder.LeagueGameFinder(team_id_nullable=hornets_id) 
-hornetgames = hornetsgamesfinder.get_data_frames()[0]  
-
-#get celtics teams stats and put into a dataframe
-nba_teams = teams.get_teams() 
-celtics = next((team for team in nba_teams if team["abbreviation"] == "BOS"), None) 
-celtics_id = hornets["id"]
-#query for games where the celtics were playing 
-celticsgamesfinder = leaguegamefinder.LeagueGameFinder(team_id_nullable=celtics_id) 
-celticsgames = celticsgamesfinder.get_data_frames()[0] 
-
-#games in 2023-2024 for hornets aganist celtics 
-season_list = ["2020", "2021", "2022", "2023"]
-hornetgames_2324 = hornetgames[hornetgames.SEASON_ID.str[-4:].isin(season_list)]    
-cha_games_2324 = hornetgames_2324[hornetgames_2324.MATCHUP.str.contains("BOS")] 
-cha_games_2324 = cha_games_2324[['SEASON_ID','TEAM_ABBREVIATION','GAME_DATE', 'MATCHUP', 'PTS', 'FGM', 'FG3M', 'REB', 'AST', 'STL', 'BLK']]
-allhornetgames = hornetgames_2324[['SEASON_ID','TEAM_ABBREVIATION','GAME_DATE', 'MATCHUP', 'PTS', 'FGM', 'FG3M', 'REB', 'AST', 'STL', 'BLK']]
-#games in 2023-2024 for celtics aganist hornets 
-season_list = ["2020", "2021", "2022", "2023"]
-celgames_2324 = celticsgames[celticsgames.SEASON_ID.str[-4:].isin(season_list)]    
-bos_games_2324 = celgames_2324[celgames_2324.MATCHUP.str.contains("CHA")] 
-bos_games_2324 = bos_games_2324[['SEASON_ID','TEAM_ABBREVIATION','GAME_DATE', 'MATCHUP', 'PTS', 'FGM', 'FG3M', 'REB', 'AST', 'STL', 'BLK']]
-allbostongames = celgames_2324[['SEASON_ID','TEAM_ABBREVIATION','GAME_DATE', 'MATCHUP', 'PTS', 'FGM', 'FG3M', 'REB', 'AST', 'STL', 'BLK']]
-
+season_list = ["2020", "2021", "2022", "2023"] 
+nba_games = pd.DataFrame() 
+for seasonid in season_list: 
+    season_log = LeagueGameLog(season=seasonid, player_or_team_abbreviation='T')  
+    season_df = season_log.get_data_frames()[0] 
+    nba_games.append(season_df)
