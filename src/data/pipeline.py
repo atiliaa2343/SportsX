967f17f3c6dfb98957e7a564f71328321178a366
atiliaa2343 <atiliaa.thomas@gmail.com>
2026-07-27 19:02:15 -0400

    fix: refactored training dataset function to identify home and away teams and grab features

--- src/data/pipeline.py
+++ src/data/pipeline.py
@@ -1,18 +1,41 @@
-from preprocess import compute_matchup_features
-from pandas import DataFrame 
 import pandas as pd
-#filtering games where it is only boston and charlotte 
-def matchup(hornet_games, celtic_games):  
-    cha_boston_games = hornet_games[hornet_games['MATCHUP'].isin(['CHA vs. BOS', "CHA @ BOS"])]
-    games_dataset = [] 
-    for game in cha_boston_games.itertuples(): 
-        game_date = game.GAME_DATE
-        features = compute_matchup_features(hornet_games, celtic_games, game_date)  
-        if features["celtics_avg_pts_last_5"] == 0 or features["hornets_avg_pts_last_5"] == 0:
-            continue
-        outcome = 1 if game.WL == "W" else 0  
-        features["outcome"] = outcome
-        games_dataset.append(features)    
-    games_dataframe = pd.DataFrame(games_dataset)
 
-    return games_dataframe
+from collect import create_nbadataframe
+from preprocess import compute_matchup_features
+
+master_dataframe = create_nbadataframe(["2020-21", "2021-22", "2022-23", "2023-24"])
+
+def training_dataset(nba_games):
+    game_ids = nba_games["GAME_ID"].unique()
+    games_dataset = []
+
+    for game_id in game_ids:
+
+        current_game = nba_games[nba_games["GAME_ID"] == game_id]
+        first_row = current_game.iloc[0]
+        current_date = first_row["GAME_DATE"]
+        matchup = first_row["MATCHUP"]
+
+        if "vs." in matchup:
+            split_teams = matchup.split(" vs. ")
+            home_team = split_teams[0].strip()
+            away_team = split_teams[1].strip()
+
+        else:
+            split_teams = matchup.split(" @ ")
+            away_team = split_teams[0].strip()
+            home_team = split_teams[1].strip()
+
+        features = compute_matchup_features(
+            home_team,
+            away_team,
+            current_date,
+            nba_games
+        )
+
+        games_dataset.append(features)
+
+    return pd.DataFrame(games_dataset)
+    
+
+        
